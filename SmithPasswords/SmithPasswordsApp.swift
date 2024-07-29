0a150cdbbbc055989e1940e4b7337409a9cade56
liam2smith <liam2004smith@gmail.com>
2024-07-29 17:07:01 -0400

    Initial Commit

---
+++ SmithPasswords/SmithPasswordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmithPasswordsApp.swift
+//  SmithPasswords
+//
+//  Created by Liam Smith on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SmithPasswordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
